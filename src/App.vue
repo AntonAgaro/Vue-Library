a6ca7e478a9e44feecb2aeb1d578ec6415878c43
Anton Agaronov <anton.agaronov@legalbet.com>
2022-10-16 17:06:23 +0400

    init: vue library init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import ProgressBar from "./components/ProgressBar.vue";
+</script>
+
+<template>
+  <main>
+    <progress-bar :done="20" />
+    <progress-bar :done="50" :reverse="true" />
+    <progress-bar :done="75" :reverse="true" />
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,43 @@
+<script lang="ts" setup>
+interface ProgressBarProps {
+  done: number;
+  reverse?: boolean;
+}
+
+const props = defineProps<ProgressBarProps>();
+</script>
+
+<template>
+  <div>
+    <div class="progress" :class="{ 'progress--revert': props.reverse }">
+      <div class="progress__done" :style="{ width: props.done + '%' }">
+        <div class="progress-text">{{ props.done }}%</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.progress {
+  width: 500px;
+  height: 50px;
+  display: flex;
+  background: #ccc;
+  border-radius: 6px;
+
+  &--revert {
+    justify-content: flex-end;
+  }
+
+  &__done {
+    height: 100%;
+    width: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: dodgerblue;
+    transition: all 0.3s ease-in-out;
+    border-radius: 6px;
+  }
+}
+</style>
